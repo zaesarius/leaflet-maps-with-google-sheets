c3c237b29ec5903a0d71038a4bf384fed264cdf3
zaesar <zaesarpo@gmail.com>
2018-11-23 12:18:01 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xnrpDw2aKkd1mCehgs2uikbuD39M2C_DDQ3CaY4iOsM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/10eMJLXLywy1flLAAmFi-cIAB6VT2Qnh58T0orkDKyZ4/edit#gid=0';
